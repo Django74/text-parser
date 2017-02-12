8fa2a1c15d6bbfad2e5e40fa13c95147ed7e6a5c
vtruon <sp_bass88@hotmail.com>
2017-02-12 01:45:08 -0700

    done 6/9, need non empty, most frequent, and longest words

--- code.js
+++ code.js
@@ -1,17 +1,111 @@
 //
-// this is just a stub for a function you need to implement
+// Vincent Truong 10137376 B05
 //
 function getStats(txt) {
-    return {
-        nChars: 123,
-        nWords: 22,
-        nLines: 10,
-        nNonEmptyLines: 22,
-        averageWordLength: 3.3,
-        maxLineLength: 33,
-        palindromes: ["12321", "kayak", "mom"],
-        longestWords: ["xxxxxxxxx", "123444444"],
-        mostFrequentWords: ["hello(7)", "world(1)"]
-    };
+	return{
+		nChars: txt.length,
+		nWords: getNumberWords(txt),
+		nLines: getNumberLines(txt),
+		nNonEmptyLines: getNonEmptyLines(txt),
+		averageWordLength: getAverageWordLength(txt),
+		maxLineLength: getMaxLineLength(txt),
+		palindromes: getPalindromes(txt),
+		longestWords: ["xxxxxxxxx", "123444444"],
+		mostFrequentWords: ["hello(7)", "world(1)"]
+	};
 }
 
+function getNumberWords(txt)
+{
+	txt = txt.replace(/[^0-9A-Z]/gi," ");
+	txt = txt.split(" ");
+	return txt.filter(function(v){return v!==''}).length;	
+}
+
+function getNumberLines(txt)
+{
+	if (txt.length == 0){
+		return 0;
+	}
+	
+	else{
+		return txt.split(/\r\n|\r|\n/).length;
+	}
+		
+}
+
+function getNonEmptyLines(txt)
+{
+	return 0;
+}
+
+function getAverageWordLength(txt)
+{
+	txt = txt.replace(/[^0-9A-Z]/gi," ");
+	txt = txt.split(" ");
+	wordArray = txt.filter(function(v){return v!==''});
+	
+	var wordCount = 0;
+	for (i = 0; i <wordArray.length; i++)
+	{
+		wordCount += wordArray[i].length;
+	}
+	
+	return wordCount/wordArray.length;
+}
+
+function getMaxLineLength(txt)
+{
+	txt = txt.replace(/\r\n/,"\n");
+	wordArray = txt.split("\n");
+	
+	var maxLength = 0;
+	
+	for (i=0; i<wordArray.length; i++)
+	{
+		if (wordArray[i].length > maxLength)
+		{
+			maxLength = wordArray[i].length;
+		}
+	}
+	
+	return maxLength;
+}
+
+function getPalindromes(txt)
+{
+	txt = txt.replace(/[^0-9A-Z]/gi," ");
+	txt = txt.split(" ");
+	wordArray = txt.filter(function(v){return v!==''});
+	
+	var palindromeArray = [];
+	for (i = 0; i <wordArray.length; i++)
+	{
+		if (isPalindrome(wordArray[i])){
+			palindromeArray.push(wordArray[i]);
+		}
+	}
+	
+	return palindromeArray;
+}
+
+function isPalindrome(txt)
+{
+	if (txt.length==0 || txt.length==1)
+	{
+		return true;
+	}
+	
+	else
+	{			
+		if (txt.length > 1 && txt.charAt(0) == txt.charAt(txt.length - 1))
+		{
+			return isPalindrome(txt.substring(1,txt.length-1));
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
+}
